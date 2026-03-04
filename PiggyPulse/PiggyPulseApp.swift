077d8ea0d6a51df17fca86c9c6fd23b05c1137d5
Leonardo Almeida <leocalm@gmail.com>
2026-03-04 17:44:44 +0100

    Initial Commit

---
+++ PiggyPulse/PiggyPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyPulseApp.swift
+//  PiggyPulse
+//
+//  Created by Leonardo Almeida on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PiggyPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
